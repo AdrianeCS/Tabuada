1af51e03bc7abd2f5b80b9fdabfa09412d20416d
adriane <adrianedacosta21@gmail.com>
2022-05-20 20:43:40 -0300

    fianlizar

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("digite o numero da tabuada...: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int contador = 0;
+int produto;
+
+while (contador <= 10)
+{
+produto = numero * contador;
+Console.WriteLine($"{numero} x {contador++} = {produto}");
+}
+return;
+
